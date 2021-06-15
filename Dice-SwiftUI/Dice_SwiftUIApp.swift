371970697c17cb21f13a66828f848c118264c9e4
Wiwi <wiwi880331@gmail.com>
2021-06-15 12:01:22 +0800

    Initial Commit

---
+++ Dice-SwiftUI/Dice_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_SwiftUIApp.swift
+//  Dice-SwiftUI
+//
+//  Created by wiwi on 2021/6/15.
+//
+
+import SwiftUI
+
+@main
+struct Dice_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
